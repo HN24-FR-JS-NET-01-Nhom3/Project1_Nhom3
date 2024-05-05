057e6425fe3e6b60e6dfd3087e44fcd480d17d2e
Chí Hiếu <hieuhc171@gmail.com>
2024-05-05 16:59:24 +0700

    update connectionString in dbContext

--- LotteryChecker.Core/Data/LotteryContext.cs
+++ LotteryChecker.Core/Data/LotteryContext.cs
@@ -1,37 +1,36 @@
 using LotteryChecker.Core.Entities;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LotteryChecker.Core.Data;
 
-public class LotteryContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+public class LotteryContext : DbContext
 {
-    //public LotteryContext(DbContextOptions options) : base(options)
-    //{
-    //}
+	public DbSet<Lottery> Lotteries { get; set; }
+	public DbSet<Reward> Rewards { get; set; }
+	public DbSet<SearchHistory> SearchHistories { get; set; }
+	public DbSet<PurchaseTicket> PurchaseTickets { get; set; }
 
-    //protected LotteryContext()
-    //{
-    //}
-
-    public DbSet<Lottery> Lotteries { get; set; }
-    public DbSet<Reward> Rewards { get; set; }
-    public DbSet<SearchHistory> SearchHistories { get; set; }
-    public DbSet<PurchaseTicket> PurchaseTickets { get; set; }
-
-    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        string connectionString = "Server=DESKTOP-38FHDTU\\SQLEXPRESS;Database=FSA_Lottery;Trusted_Connection=True;TrustServerCertificate=True";
-        base.OnConfiguring(optionsBuilder);
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-    }
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Seed();
-        base.OnModelCreating(builder);
-    }
+	// public LotteryContext(DbContextOptions options) : base(options)
+	// {
+	// }
+	//
+	// protected LotteryContext()
+	// {
+	// }
+    
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		string connectionString = "Server=.;Database=FSA_Lottery;Trusted_Connection=True;TrustServerCertificate=True";
+		base.OnConfiguring(optionsBuilder);
+		if (!optionsBuilder.IsConfigured)
+		{
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+	}
+    
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.Seed();
+		base.OnModelCreating(builder);
+	}
 }
