38967e28306075e202a0398c6a359ba265ff611d
Chí Hiếu <hieuhc171@gmail.com>
2024-05-06 09:47:04 +0700

    init AutoMapper

---
+++ LotteryChecker.MVC/AutoMapper/AutoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LotteryChecker.Core.Entities;
+using LotteryChecker.MVC.Models.Entities;
+
+namespace LotteryChecker.MVC.AutoMapper;
+
+public class AutoMapper : Profile
+{
+	public AutoMapper()
+	{
+		CreateMap<Lottery, LotteryVm>().ReverseMap();
+		CreateMap<PurchaseTicket, PurchaseTicketVm>().ReverseMap();
+		CreateMap<Reward, RewardVm>().ReverseMap();
+	}
+}
---
+++ LotteryChecker.MVC/Models/Entities/PurchaseTicketVm.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LotteryChecker.MVC.Models.Entities;
+
+public class PurchaseTicketVm
+{
+	[Required(ErrorMessage = "Purchase date is required")]
+	public DateTime PurchaseDate { get; set; }
+        
+	[Required(ErrorMessage = "Lottery number is required")]
+	[RegularExpression(@"^\d{5}$", ErrorMessage = "Lottery number must be a 5-digit number")]
+	public int LotteryNumber { get; set; }
+        
+	[Required(ErrorMessage = "User is required")]
+	public Guid UserId { get; set; }
+}
