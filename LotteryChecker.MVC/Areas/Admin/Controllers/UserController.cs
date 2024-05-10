662d84c5a391a66712ac37b9109f02405bd7a07c
Chí Hiếu <hieuhc171@gmail.com>
2024-05-10 14:57:11 +0700

    authorize

--- LotteryChecker.MVC/Areas/Admin/Controllers/UserController.cs
+++ LotteryChecker.MVC/Areas/Admin/Controllers/UserController.cs
@@ -2,136 +2,161 @@
 using LotteryChecker.Common.Models.ViewModels;
 using LotteryChecker.MVC.Models;
 using LotteryChecker.MVC.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace LotteryChecker.MVC.Areas.Admin.Controllers
+namespace LotteryChecker.MVC.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("admin/user")]
+[Authorize(Roles = "Admin")]
+public class UserController : Controller
 {
-    [Area("Admin")]
-    [Route("admin/user")]
-    public class UserController : Controller
+    private readonly IMapper _mapper;
+
+    public UserController(IMapper mapper)
     {
-        private readonly IMapper _mapper;
-
-        public UserController(IMapper mapper)
-        {
-            _mapper = mapper;
-        }
+        _mapper = mapper;
+    }
         
-        [Route("get-all-users")]
-        [Route("get-all-users/page={page}&pageSize={pageSize}")]
-        public async Task<IActionResult> Index(int page = 1, int pageSize = 5)
+    [HttpGet]
+    [Route("get-all-users")]
+    [Route("get-all-users/{page}/{pageSize}")]
+    public async Task<IActionResult> Index(int page = 1, int pageSize = 5)
+    {
+        try
         {
-            try
-            {
-                var response = await HttpUtils<UserPagingVm>.SendRequest(HttpMethod.Get,
+            var response = await HttpUtils<UserPagingVm>.SendRequest(HttpMethod.Get,
                 $"{Constants.API_USER}/get-all-users/page={page}&pageSize={pageSize}");
-                if (response != null)
-                    return View(response);
-                else
-                    return View();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+            if (response != null)
+                return View(response);
+            else
                 return View();
-            }
         }
-
-        [Route("get-user/{id}")]
-        public async Task<IActionResult> Detail(string id)
+        catch (Exception ex)
         {
-            try
+            Console.WriteLine(ex);
+            return View();
+        }
+    }
+
+    [Route("get-user/{id}")]
+    public async Task<IActionResult> Detail(string id)
+    {
+        try
+        {
+            var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Get,
+                $"{Constants.API_USER}/get-user/{id}");
+            if (respone != null)
+                return View(respone);
+            else
+                return View();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return View();
+        }
+    }
+
+    [HttpGet]
+    [Route("edit-user/{id}")]
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        try
+        {
+            var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Get,
+                $"{Constants.API_USER}/get-user/{id}");
+            if (respone != null)
+                return View(respone);
+            else
+                return View();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return View();
+        }
+    }
+
+    [HttpPost]
+    [Route("edit-user/{id}")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(UserVm? userVm, Guid id)
+    {
+        try
+        {
+            if (ModelState.IsValid)
             {
-                var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Get,
-                    $"{Constants.API_USER}/get-user/{id}");
+                var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Put,
+                    $"{Constants.API_USER}/update-user/{id}", userVm);
                 if (respone != null)
-                    return View(respone);
-                else
-                    return View();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return View();
-            }
-        }
-
-        [HttpGet]
-        [Route("edit-user/{id}")]
-        public async Task<IActionResult> Edit(Guid id)
-        {
-            try
-            {
-                var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Get,
-                    $"{Constants.API_USER}/get-user/{id}");
-                if (respone != null)
-                    return View(respone);
-                else
-                    return View();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return View();
-            }
-        }
-
-        [HttpPost]
-        [Route("edit-user/{id}")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(UserVm? userVm, Guid id)
-        {
-            try
-            {
-                if (ModelState.IsValid)
                 {
-                    var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Put,
-                         $"{Constants.API_USER}/update-user/{id}", userVm);
-                    if (respone != null)
-                    {
-                        return RedirectToAction("Index");
-                    }
-                    else
-                        return View();
+                    return RedirectToAction("Index");
                 }
-                return View();
+                else
+                    return View();
             }
-            catch (Exception ex)
-            {
-                return View();
-            }
-
+            return View();
         }
-
-        [Route("create-user")]
-        public IActionResult Create()
+        catch (Exception ex)
         {
             return View();
         }
 
-        [HttpPost]
-        [Route("create-user")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(CreateUserVm? userVm)
+    }
+
+    [Route("create-user")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("create-user")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CreateUserVm? userVm)
+    {
+        try
         {
-            try
+            if(ModelState.IsValid)
             {
-                if(ModelState.IsValid)
-                {
-                    var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Post,
-                        $"{Constants.API_USER}/create-user", userVm);
-                    if (respone != null)
-                        return RedirectToAction("Index");
-                    else
-                        return View();
-                }
-                return View();
+                var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Post,
+                    $"{Constants.API_USER}/create-user", userVm);
+                if (respone != null)
+                    return RedirectToAction("Index");
+                else
+                    return View();
             }
-            catch (Exception ex)
+            return View();
+        }
+        catch (Exception ex)
+        {
+            ViewBag.ErrorMessage = ex.Message;
+            return View();
+        }
+    }
+
+    [HttpPost]
+    [Route("change-status-user/{id}/{isActive}")]
+    public async Task<IActionResult> ChangeStatusUser(string id, bool isActive)
+    {
+        try
+        {
+            var respone = await HttpUtils<UserVm>.SendRequest(HttpMethod.Post,
+                $"{Constants.API_USER}/update-status-user/{id}/{!isActive}");
+            if (respone != null)
             {
-                ViewBag.ErrorMessage = ex.Message;
-                return View();
+                TempData["SuccessMessage"] = "Changed status successfully";
+                Console.WriteLine("ok");
             }
+            return RedirectToAction("Index");
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = JsonConvert.DeserializeObject<ErrorVm>(ex.Message);
+            return RedirectToAction("Index");
         }
     }
 }
