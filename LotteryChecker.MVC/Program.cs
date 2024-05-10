49404e7f8175670d9717129444f1b645a4c94beb
Chí Hiếu <hieuhc171@gmail.com>
2024-05-10 20:54:05 +0700

    add error page

--- LotteryChecker.MVC/Program.cs
+++ LotteryChecker.MVC/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddAuthentication(options =>
 	})
 	.AddCookie("Cookies", options =>
 	{
+		options.AccessDeniedPath = "/home/error";
 		options.LoginPath = "/authen/login";
 	});
 
