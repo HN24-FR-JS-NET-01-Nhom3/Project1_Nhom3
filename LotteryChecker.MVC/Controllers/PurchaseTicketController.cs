2aa5a8aac0ecd67f48254161b5e4e373d0a45998
hiu_cd <hovanhieu2012sch@gmail.com>
2024-05-12 22:39:13 +0700

    purchase ticket (history)

--- LotteryChecker.MVC/Controllers/PurchaseTicketController.cs
+++ LotteryChecker.MVC/Controllers/PurchaseTicketController.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using LotteryChecker.Common.Models.Authentications;
 using LotteryChecker.Common.Models.Entities;
+using LotteryChecker.Core.Entities;
 using LotteryChecker.MVC.Models;
 using LotteryChecker.MVC.Utils;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 namespace LotteryChecker.MVC.Controllers
 {
@@ -10,30 +16,75 @@ namespace LotteryChecker.MVC.Controllers
     public class PurchaseTicketController : Controller
     {
         private readonly IMapper _mapper;
+        private readonly HttpClient _httpClient;
 
         public PurchaseTicketController(IMapper mapper)
         {
+            _httpClient = new HttpClient();
             _mapper = mapper;
         }
 
         [Route("")]
-    
+
         public async Task<IActionResult> Index()
         {
             try
             {
-                var purchaseTicketResponse = await HttpUtils<IEnumerable<PurchaseTicketVm>>.SendRequest(HttpMethod.Get, $"{Constants.API_PURCHASE_TICKET}/get-all-purchase-tickets");
+    
+                var apiResponse = await _httpClient.GetAsync("https://localhost:7178/api/v1/purchase-ticket/get-all-purchase-tickets");
 
-                if (purchaseTicketResponse == null) purchaseTicketResponse = [];
-                var purchaseTicketResult = purchaseTicketResponse.ToList();
+                if (apiResponse.IsSuccessStatusCode)
+                {
+           
+                    var responseContent = await apiResponse.Content.ReadAsStringAsync();
 
-                return View(purchaseTicketResult);
+          
+                    var response = JsonConvert.DeserializeObject<HttpResponse<PurchaseTicketVm>>(responseContent);
 
+             
+                    return View(response.Result);
+                }
+                else if (apiResponse.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return NotFound();
+                }
+                else
+                {
+      
+                    var errorMessage = await apiResponse.Content.ReadAsStringAsync();
+                    return StatusCode((int)apiResponse.StatusCode, errorMessage);
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
-                throw;
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("create")]
+
+        public async Task<IActionResult> Create(PurchaseTicket purchaseTicket)
+        {
+            try
+            {
+                var purchaseTicketJson = JsonConvert.SerializeObject(purchaseTicket);
+                var content = new StringContent(purchaseTicketJson, Encoding.UTF8, "application/json");
+
+                var apiResponse = await _httpClient.PostAsync("https://localhost:7178/api/v1/purchase-ticket/create-purchase-ticket", content);
+
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var errorMessage = await apiResponse.Content.ReadAsStringAsync();
+                    return StatusCode((int)apiResponse.StatusCode, errorMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
             }
         }
 
